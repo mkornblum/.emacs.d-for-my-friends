37c9bd0c52263592c42a35b17760576553bbb5fa
Mark Kornblum <mark.kornblum@gmail.com>
2018-08-21 21:02:33 -0700

    Setup lsp-java for some IDE-lite type functionality

--- customizations.el
+++ customizations.el
@@ -3,10 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(create-lockfiles nil)
  '(flycheck-javascript-flow-args nil)
  '(global-highline-mode t)
  '(inhibit-startup-screen t)
  '(js-indent-level 2)
+ '(lsp-ui-sideline-show-code-actions nil)
+ '(lsp-ui-sideline-show-hover nil)
+ '(lsp-ui-sideline-show-symbol nil)
  '(magit-bury-buffer-function (quote magit-mode-quit-window))
  '(magit-diff-use-overlays nil)
  '(ns-command-modifier (quote meta))
@@ -17,7 +21,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (htmlize ox-reveal flow-minor-mode wgrep company-flow avy git-timemachine delight dired web-mode visible-mark use-package tide solarized-theme smex smart-indent-rigidly slime simp sass-mode rjsx-mode prettier-js powerline multiple-cursors move-dup markdown-mode magit lua-mode itail isearch-symbol-at-point ido-completing-read+ highline flycheck-flow expand-region exec-path-from-shell eslintd-fix emojify elm-mode dired-efap counsel company auto-indent-mode auto-complete add-node-modules-path)))
+    (lsp-ui lsp-java company-lsp htmlize ox-reveal flow-minor-mode wgrep company-flow avy git-timemachine delight dired web-mode visible-mark use-package tide solarized-theme smex smart-indent-rigidly slime simp sass-mode rjsx-mode prettier-js powerline multiple-cursors move-dup markdown-mode magit lua-mode itail isearch-symbol-at-point ido-completing-read+ highline flycheck-flow expand-region exec-path-from-shell eslintd-fix emojify elm-mode dired-efap counsel company auto-indent-mode auto-complete add-node-modules-path)))
  '(show-paren-mode t)
  '(typescript-indent-level 2)
  '(web-mode-code-indent-offset 2)
--- init.el
+++ init.el
@@ -84,7 +84,6 @@
     (flycheck-add-mode 'javascript-flow 'web-mode)
     (flycheck-add-mode 'javascript-flow 'rjsx-mode)
     (flycheck-add-next-checker 'javascript-flow 'javascript-eslint)))
-
 (use-package git-timemachine)
 (use-package highline
   :delight global-highline-mode)
@@ -94,6 +93,37 @@
 (use-package isearch-symbol-at-point)
 (use-package flow-minor-mode)
 (use-package lua-mode)
+(use-package lsp-mode
+  :ensure t
+  :init (setq lsp-inhibit-message t
+              lsp-eldoc-render-all nil
+              lsp-highlight-symbol-at-point nil))
+(use-package company-lsp
+  :after  company
+  :ensure t
+  :config
+  (add-hook 'java-mode-hook (lambda () (push 'company-lsp company-backends)))
+  (setq company-lsp-enable-snippet t
+        company-lsp-cache-candidates t)
+  (push 'java-mode company-global-modes))
+(use-package lsp-ui
+  :ensure t
+  :config
+  (setq lsp-ui-sideline-enable t
+        lsp-ui-sideline-show-symbol t
+        lsp-ui-sideline-show-hover t
+        lsp-ui-sideline-show-code-actions t
+        lsp-ui-sideline-update-mode 'point))
+(use-package lsp-java
+  :ensure t
+  :requires (lsp-ui-flycheck lsp-ui-sideline)
+  :config
+  (add-hook 'java-mode-hook  'lsp-java-enable)
+  (add-hook 'java-mode-hook  'flycheck-mode)
+  (add-hook 'java-mode-hook  'company-mode)
+  (add-hook 'java-mode-hook  (lambda () (lsp-ui-flycheck-enable t)))
+  (add-hook 'java-mode-hook  'lsp-ui-sideline-mode)
+  (setq lsp-java--workspace-folders (list "/Users/mark/code/java-koans/koans/src")))
 (use-package markdown-mode)
 (use-package magit
   :bind ("M-j g" . magit-status))
